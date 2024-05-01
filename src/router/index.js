e76a39cf07e14ca4d1dd970b9758ad05ad61d23b
tictools <jaumevillarreal@gmail.com>
2024-05-01 09:41:04 +0200

    feat: add landingRouter

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import healthRouterIoC from "./health/healthRouter.js";
+import landingRouterIoC from "./landing/landingRouter.js";
 import notesRouterIoC from "./notes/notesRouter.js";
 
-export { healthRouterIoC, notesRouterIoC };
+export { healthRouterIoC, landingRouterIoC, notesRouterIoC };
