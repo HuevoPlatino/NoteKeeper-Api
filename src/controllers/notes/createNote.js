7de0bcc3173e49ddd9994bccf9c042b60989bf13
tictools <jaumevillarreal@gmail.com>
2024-05-02 11:07:28 +0200

    feat: add data validation

--- src/controllers/notes/createNote.js
+++ src/controllers/notes/createNote.js
@@ -1,14 +1,21 @@
 import { noteMapper } from "../../mappers/index.js";
+import { checkNoteDataIsIncomplete } from "../../router/notes/helpers/index.js";
 import { generatePrefixedID } from "../../utils/index.js";
 
 const createNote = (repository) => {
   return (req, res) => {
     const note = req.body;
 
+    const isNoteDataIncomplete = checkNoteDataIsIncomplete(note);
+
+    if (isNoteDataIncomplete) {
+      return res.status(400).json({ error: "Missing data." });
+    }
+
     const newNote = {
       ...note,
       _id: generatePrefixedID(),
-      created_at: Date.now(),
+      created_at: Date.now()
     };
 
     repository.addNote(newNote);
