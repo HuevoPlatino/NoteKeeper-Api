3bca0caedc6dfdfd9d7ad45fa7dfe3850fc01ddc
tictools <jaumevillarreal@gmail.com>
2024-05-04 12:37:46 +0200

    feat: remove brackets

--- src/notes/repositories/NotesInMemoryRepository.js
+++ src/notes/repositories/NotesInMemoryRepository.js
@@ -1,33 +1,31 @@
 import notes from "../data/index.js";
 
-export const NotesInMemoryRepository = () => {
-  return {
-    getAllNotes: async () => {
-      return notes;
-    },
+export const NotesInMemoryRepository = () => ({
+  getAllNotes: async () => {
+    return notes;
+  },
 
-    getNote: async (index) => {
-      return notes[index];
-    },
+  getNote: async (index) => {
+    return notes[index];
+  },
 
-    addNote: async (note) => {
-      notes.push(note);
-    },
+  addNote: async (note) => {
+    notes.push(note);
+  },
 
-    updateNote: async (index, note) => {
-      notes[index] = note;
-    },
+  updateNote: async (index, note) => {
+    notes[index] = note;
+  },
 
-    removeNote: async (index) => {
-      return notes.splice(index, 1);
-    },
+  removeNote: async (index) => {
+    return notes.splice(index, 1);
+  },
 
-    findIndexNoteById: async (noteId) => {
-      return notes.findIndex((note) => note._id === noteId);
-    },
+  findIndexNoteById: async (noteId) => {
+    return notes.findIndex((note) => note._id === noteId);
+  },
 
-    removeAllNotes: async () => {
-      notes.length = 0;
-    }
-  };
-};
+  removeAllNotes: async () => {
+    notes.length = 0;
+  }
+});
