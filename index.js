76c9f3a962804a37270768a4c8d92ff266082691
tictools <jaumevillarreal@gmail.com>
2024-05-06 00:22:31 +0200

    feat: move repository dependency inversion to app root

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ import healthRouterIoC from "./src/health/router/healthRouter.js";
 import landingRouterIoC from "./src/landing/router/landingRouter.js";
 import notesRouterIoC from "./src/notes/router/notesRouter.js";
 
+import NotesInMemoryRepository from "./src/notes/repository/NotesInMemoryRepository.js";
 import { Logger } from "./src/utils/index.js";
 
 const PORT = process.env.PORT ?? 3001;
 const appVersion = process.env.npm_package_version;
+
+const notesInMemoryRepository = NotesInMemoryRepository();
 
 const app = express();
 app.use(express.json());
@@ -17,7 +20,7 @@ app.use(cors());
 
 landingRouterIoC(app, appVersion);
 healthRouterIoC(app);
-notesRouterIoC(app);
+notesRouterIoC(app, notesInMemoryRepository);
 
 app.listen(PORT, () => {
   Logger.lineDivider();
